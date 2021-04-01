eb304b2340c921dc0a13f9917adace57d7d3c386
王晓敏 <wangxiaomin@xiaoyezi.com>
2021-04-01 23:46:21 +0800

    fix

--- .history/src/views/tmk/index_20210401233911.vue
+++ .history/src/views/tmk/index_20210401233911.vue
@@ -19,14 +19,15 @@
           <el-col :span="6">
             <el-form-item label="激活级别">
               <el-select
-                v-model="form.level"
+                v-model="value"
                 placeholder="请选择"
+                @change="selectChange($event)"
               >
                 <el-option
                   v-for="item in options"
                   :key="item.value"
-                  :label="item.value"
-                  :value="item.code"
+                  :label="item.label"
+                  :value="item.value"
                 />
               </el-select>
             </el-form-item>
@@ -36,14 +37,15 @@
           <el-col :span="6" :offset="2">
             <el-form-item label="分配类型">
               <el-select
-                v-model="form.type"
+                v-model="valueTypeAll"
                 placeholder="请选择"
+                @change="selectChange1($event)"
               >
                 <el-option
                   v-for="item in typeAll"
                   :key="item.value"
-                  :label="item.value"
-                  :value="item.code"
+                  :label="item.label"
+                  :value="item.value"
                 />
               </el-select>
             </el-form-item>
@@ -51,14 +53,15 @@
           <el-col :span="6">
             <el-form-item label="分配角色">
               <el-select
-                v-model="form.roles"
+                v-model="valueRoles"
                 placeholder="请选择"
+                @change="selectChange2($event)"
               >
                 <el-option
                   v-for="item in roles"
                   :key="item.value"
-                  :label="item.value"
-                  :value="item.code"
+                  :label="item.label"
+                  :value="item.value"
                 />
               </el-select>
             </el-form-item>
@@ -66,14 +69,15 @@
           <el-col :span="6">
             <el-form-item label="自约课是否直分cc">
               <el-select
-                v-model="form.isCC"
+                v-model="valueOptionsCC"
                 placeholder="请选择"
+                @change="selectChange3($event)"
               >
                 <el-option
                   v-for="item in optionsCC"
                   :key="item.value"
-                  :label="item.value"
-                  :value="item.code"
+                  :label="item.label"
+                  :value="item.value"
                 />
               </el-select>
             </el-form-item>
@@ -83,26 +87,27 @@
           <el-col :span="6" :offset="2">
             <el-form-item label="状态">
               <el-select
-                v-model="form.status"
+                v-model="valueState"
                 placeholder="请选择"
+                @change="selectChange4($event)"
               >
                 <el-option
                   v-for="item in state"
                   :key="item.value"
-                  :label="item.value"
-                  :value="item.code"
+                  :label="item.label"
+                  :value="item.value"
                 />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="6">
             <el-form-item label="探索颜色">
-              <el-select v-model="form.color" placeholder="请选择">
+              <el-select v-model="valuecolor" placeholder="请选择">
                 <el-option
                   v-for="item in color"
                   :key="item.value"
-                  :label="item.value"
-                  :value="item.code"
+                  :label="item.label"
+                  :value="item.value"
                 />
               </el-select>
             </el-form-item>
@@ -235,15 +240,115 @@ export default {
         level: '',
         type: '',
         roles: '',
-        isCC: '',
-        status: '',
-        color: ''
+        
       },
-      tableData: []
+      // value: '',
+      valueTypeAll: '',
+      valueRoles: '',
+      valueOptionsCC: '',
+      valueState: '',
+      valuecolor: '',
+
+      newtableData: [],
+      newtableData1: [],
+      tableData: [
+        {
+          allocate_role: '2',
+          allocate_role_str: '分配CC', // 分配角色
+          auto_allocate: '1',
+          auto_allocate_str: '是', // 自约课是否直分CC
+          color_index: '1',
+          color_index_str: 'rgba(135,206,235,0.4)',
+          create_time: '1615974624',
+          id: '26', // ID
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配', // 分配类型
+          level: 'S', // 激活级别
+          name: '智能待分配手动分配测试激活方式', // 激活方式名称
+          operator_id: '11089',
+          sales_talk: '激活花束', // 话术
+          status: '1',
+          status_str: '已启用', // 状态
+          update_time: '0'
+        },
+        {
+          allocate_role: '1',
+          allocate_role_str: '分配TMK',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '1',
+          color_index_str: 'rgba(135,206,235,0.4)',
+          create_time: '1615201819',
+          id: '25',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'A',
+          name: '测试用-勿动01',
+          operator_id: '11068',
+          sales_talk: '测试用-勿动01',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        },
+        {
+          allocate_role: '2',
+          allocate_role_str: '分配CC',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '4',
+          color_index_str: 'rgba(152,251,152,0.4)',
+          create_time: '1614846757',
+          id: '23',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'S',
+          name: '验收激活01',
+          operator_id: '10989',
+          sales_talk: '测试验收',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        },
+        {
+          allocate_role: '2',
+          allocate_role_str: '分配CC',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '4',
+          color_index_str: 'rgba(152,251,152,0.4)',
+          create_time: '1614236426',
+          id: '22',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'A',
+          name: 'B级别的激活方式激活历史',
+          operator_id: '11069',
+          sales_talk: 'b级别+激活历史+tmk+立即分配',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        },
+        {
+          allocate_role: '2',
+          allocate_role_str: '分配CC',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '3',
+          color_index_str: 'rgba(240,128,128,0.4)',
+          create_time: '1614221265',
+          id: '21',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'S',
+          name: '多次激活学员激活历史测试',
+          operator_id: '11069',
+          sales_talk: '这是测试激活历史的数据',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        }
+      ]
     }
-  },
-  created() {
-    this.getTableData()
   },
   methods: {
     handleClose(done) {
@@ -254,115 +359,41 @@ export default {
         .catch((_) => {})
     },
     handleChang() {
-      const newArr = []
+      // console.log(this.form.id);
+      // console.log(this.form.name);
+      // console.log(this.value);
+      // console.log(this.valueTypeAll);
+      // console.log(this.valueRoles);
+      // console.log(this.valueOptionsCC);
+      // console.log(this.valueState);
+      // const newtableData1 = JSON.parse(JSON.stringify(this.tableData))
       this.tableData.forEach((item) => {
-        if (item.id === this.form.id) {
-          newArr.push(item)
-          this.tableData = newArr
-        }
+        // if (
+        //   this.form.id == item.id &&
+        //   this.form.name == item.name &&
+        //   this.value == item.level &&
+        //   this.valueTypeAll == item.is_delay_allocate_str &&
+        //   this.valueRoles == item.allocate_role_str &&
+        //   this.valueOptionsCC == item.auto_allocate_str &&
+        //   this.valueState == item.status_str
+        // ) {
+        //   console.log(item)
+        //   this.newtableData.push(item)
+        //   console.log(this.newtableData)
+        //   this.tableData = this.newtableData
+        // }
       })
     },
     handleChangAll() {
-      this.form = {}
-      this.getTableData()
-    },
-    getTableData() {
-      const tableData = [{
-        allocate_role: '2',
-        allocate_role_str: '分配CC', // 分配角色
-        auto_allocate: '1',
-        auto_allocate_str: '是', // 自约课是否直分CC
-        color_index: '1',
-        color_index_str: 'rgba(135,206,235,0.4)',
-        create_time: '1615974624',
-        id: '26', // ID
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配', // 分配类型
-        level: 'S', // 激活级别
-        name: '智能待分配手动分配测试激活方式', // 激活方式名称
-        operator_id: '11089',
-        sales_talk: '激活花束', // 话术
-        status: '1',
-        status_str: '已启用', // 状态
-        update_time: '0'
-      },
-      {
-        allocate_role: '1',
-        allocate_role_str: '分配TMK',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '1',
-        color_index_str: 'rgba(135,206,235,0.4)',
-        create_time: '1615201819',
-        id: '25',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'A',
-        name: '测试用-勿动01',
-        operator_id: '11068',
-        sales_talk: '测试用-勿动01',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      },
-      {
-        allocate_role: '2',
-        allocate_role_str: '分配CC',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '4',
-        color_index_str: 'rgba(152,251,152,0.4)',
-        create_time: '1614846757',
-        id: '23',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'S',
-        name: '验收激活01',
-        operator_id: '10989',
-        sales_talk: '测试验收',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      },
-      {
-        allocate_role: '2',
-        allocate_role_str: '分配CC',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '4',
-        color_index_str: 'rgba(152,251,152,0.4)',
-        create_time: '1614236426',
-        id: '22',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'A',
-        name: 'B级别的激活方式激活历史',
-        operator_id: '11069',
-        sales_talk: 'b级别+激活历史+tmk+立即分配',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      },
-      {
-        allocate_role: '2',
-        allocate_role_str: '分配CC',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '3',
-        color_index_str: 'rgba(240,128,128,0.4)',
-        create_time: '1614221265',
-        id: '21',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'S',
-        name: '多次激活学员激活历史测试',
-        operator_id: '11069',
-        sales_talk: '这是测试激活历史的数据',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      }]
-      this.tableData = tableData
+      this.form.id = ''
+      this.form.name = ''
+      this.value = ''
+      this.valueTypeAll = ''
+      this.valueRoles = ''
+      this.valueOptionsCC = ''
+      this.valueState = ''
+      // console.log(this.newtableData1)
+      // this.tableData = this.newtableData1
     }
   }
 }
--- .history/src/views/tmk/index_20210401234324.vue
+++ .history/src/views/tmk/index_20210401234324.vue
@@ -242,9 +242,6 @@ export default {
       tableData: []
     }
   },
-  created() {
-    this.getTableData()
-  },
   methods: {
     handleClose(done) {
       this.$confirm('确认关闭？')
@@ -254,7 +251,7 @@ export default {
         .catch((_) => {})
     },
     handleChang() {
-      const newArr = []
+      let newArr = []
       this.tableData.forEach((item) => {
         if (item.id === this.form.id) {
           newArr.push(item)
@@ -264,105 +261,106 @@ export default {
     },
     handleChangAll() {
       this.form = {}
-      this.getTableData()
+    
     },
     getTableData() {
-      const tableData = [{
-        allocate_role: '2',
-        allocate_role_str: '分配CC', // 分配角色
-        auto_allocate: '1',
-        auto_allocate_str: '是', // 自约课是否直分CC
-        color_index: '1',
-        color_index_str: 'rgba(135,206,235,0.4)',
-        create_time: '1615974624',
-        id: '26', // ID
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配', // 分配类型
-        level: 'S', // 激活级别
-        name: '智能待分配手动分配测试激活方式', // 激活方式名称
-        operator_id: '11089',
-        sales_talk: '激活花束', // 话术
-        status: '1',
-        status_str: '已启用', // 状态
-        update_time: '0'
-      },
-      {
-        allocate_role: '1',
-        allocate_role_str: '分配TMK',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '1',
-        color_index_str: 'rgba(135,206,235,0.4)',
-        create_time: '1615201819',
-        id: '25',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'A',
-        name: '测试用-勿动01',
-        operator_id: '11068',
-        sales_talk: '测试用-勿动01',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      },
-      {
-        allocate_role: '2',
-        allocate_role_str: '分配CC',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '4',
-        color_index_str: 'rgba(152,251,152,0.4)',
-        create_time: '1614846757',
-        id: '23',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'S',
-        name: '验收激活01',
-        operator_id: '10989',
-        sales_talk: '测试验收',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      },
-      {
-        allocate_role: '2',
-        allocate_role_str: '分配CC',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '4',
-        color_index_str: 'rgba(152,251,152,0.4)',
-        create_time: '1614236426',
-        id: '22',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'A',
-        name: 'B级别的激活方式激活历史',
-        operator_id: '11069',
-        sales_talk: 'b级别+激活历史+tmk+立即分配',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      },
-      {
-        allocate_role: '2',
-        allocate_role_str: '分配CC',
-        auto_allocate: '1',
-        auto_allocate_str: '是',
-        color_index: '3',
-        color_index_str: 'rgba(240,128,128,0.4)',
-        create_time: '1614221265',
-        id: '21',
-        is_delay_allocate: '0',
-        is_delay_allocate_str: '立即分配',
-        level: 'S',
-        name: '多次激活学员激活历史测试',
-        operator_id: '11069',
-        sales_talk: '这是测试激活历史的数据',
-        status: '1',
-        status_str: '已启用',
-        update_time: '0'
-      }]
-      this.tableData = tableData
+      let tableData = [{
+          allocate_role: '2',
+          allocate_role_str: '分配CC', // 分配角色
+          auto_allocate: '1',
+          auto_allocate_str: '是', // 自约课是否直分CC
+          color_index: '1',
+          color_index_str: 'rgba(135,206,235,0.4)',
+          create_time: '1615974624',
+          id: '26', // ID
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配', // 分配类型
+          level: 'S', // 激活级别
+          name: '智能待分配手动分配测试激活方式', // 激活方式名称
+          operator_id: '11089',
+          sales_talk: '激活花束', // 话术
+          status: '1',
+          status_str: '已启用', // 状态
+          update_time: '0'
+        },
+        {
+          allocate_role: '1',
+          allocate_role_str: '分配TMK',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '1',
+          color_index_str: 'rgba(135,206,235,0.4)',
+          create_time: '1615201819',
+          id: '25',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'A',
+          name: '测试用-勿动01',
+          operator_id: '11068',
+          sales_talk: '测试用-勿动01',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        },
+        {
+          allocate_role: '2',
+          allocate_role_str: '分配CC',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '4',
+          color_index_str: 'rgba(152,251,152,0.4)',
+          create_time: '1614846757',
+          id: '23',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'S',
+          name: '验收激活01',
+          operator_id: '10989',
+          sales_talk: '测试验收',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        },
+        {
+          allocate_role: '2',
+          allocate_role_str: '分配CC',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '4',
+          color_index_str: 'rgba(152,251,152,0.4)',
+          create_time: '1614236426',
+          id: '22',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'A',
+          name: 'B级别的激活方式激活历史',
+          operator_id: '11069',
+          sales_talk: 'b级别+激活历史+tmk+立即分配',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        },
+        {
+          allocate_role: '2',
+          allocate_role_str: '分配CC',
+          auto_allocate: '1',
+          auto_allocate_str: '是',
+          color_index: '3',
+          color_index_str: 'rgba(240,128,128,0.4)',
+          create_time: '1614221265',
+          id: '21',
+          is_delay_allocate: '0',
+          is_delay_allocate_str: '立即分配',
+          level: 'S',
+          name: '多次激活学员激活历史测试',
+          operator_id: '11069',
+          sales_talk: '这是测试激活历史的数据',
+          status: '1',
+          status_str: '已启用',
+          update_time: '0'
+        }]
+
+      this.tableData = tableData 
     }
   }
 }
